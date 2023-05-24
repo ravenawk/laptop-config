4cca836be0397451c667fdb52cf2666c68e6db9e
Pat Martin <wpmartin@gmail.com>
2023-05-24 11:30:22 -0700

    Updated setup.sh to check for MacOS

--- setup.sh
+++ setup.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    OS=$NAME
-fi
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	if [ -f /etc/os-release ]; then
+	    . /etc/os-release
+	    OS=$NAME
+	fi
 
-shopt -s nocasematch
+	shopt -s nocasematch
 
-case $OS in
-    *deb* | *pop* | *ubu*)
-        FAMILY="Debian";;
-    *fed* | *red*)
-        FAMILY="RedHat";;
-esac
+	case $OS in
+	    *deb* | *pop* | *ubu*)
+		FAMILY="Debian";;
+	    *fed* | *red*)
+		FAMILY="RedHat";;
+	esac
 
-if [[ $FAMILY == "RedHat" ]]; then
-    if !(rpm -qa | grep -q ansible); then
-        sudo dnf -y install ansible
-    fi
-fi
+	if [[ $FAMILY == "RedHat" ]]; then
+	    if !(rpm -qa | grep -q ansible); then
+		sudo dnf -y install ansible
+	    fi
+	fi
 
-if [[ $FAMILY == "Debian" ]]; then
-    if !(apt list --installed | grep -q ansible); then
-        sudo apt -y install ansible
-    fi
+	if [[ $FAMILY == "Debian" ]]; then
+	    if !(apt list --installed | grep -q ansible); then
+		sudo apt -y install ansible
+	    fi
+	fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	FAMILY=MacOS	
 fi
 
 ansible-playbook site.yml -e FAMILY=$FAMILY
